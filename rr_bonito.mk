b9a777cb6739b625c605d75e316074d50ca79d4a
ProtoAES256 <xiangyann@hotmail.com>
2019-12-11 00:53:45 +0800

    lineage to rr fixes

--- rr_bonito.mk
+++ rr_bonito.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/bonito/aosp_bonito.mk)
